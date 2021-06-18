a4c21eee1877e0a3f8693c27df0d37a2ae2b23cf
alinkkh <alinkkh9@gmail.com>
2021-06-18 18:31:17 +0430

    BugFix

--- src/main/java/control/Level.java
+++ src/main/java/control/Level.java
@@ -4,17 +4,18 @@ import model.Animal;
 import model.Facility;
 import model.Threat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Level {
     private int level,goldTime,silverTime,bronzeTime,goldStars,silverStars,bronzeStars;
-    private HashMap<Integer,Animal> AnimalCycle;
-    private HashMap<Integer, Threat> ThreatCycle;
-    private HashMap<Integer, Facility> FacilityCycle;
+    private ArrayList<Animal> AnimalCycle;
+    private ArrayList<Facility> FacilityCycle;
+    private HashMap<Integer, Enum> ThreatCycle;
     private HashMap<Enum,Integer> LevelGoals;
     // All data of a level should be here
 
-    public Level(int level, int goldTime, int silverTime, int bronzeTime, int goldStars, int silverStars, int bronzeStars,HashMap animalCycle,HashMap threatCycle,HashMap facilityCycle,HashMap levelGoals) {
+    public Level(int level, int goldTime, int silverTime, int bronzeTime, int goldStars, int silverStars, int bronzeStars,ArrayList animalCycle,HashMap threatCycle,ArrayList facilityCycle,HashMap levelGoals) {
         this.level = level;
         this.goldTime = goldTime;
         this.silverTime = silverTime;
@@ -84,29 +85,9 @@ public class Level {
         this.bronzeStars = bronzeStars;
     }
 
-    public HashMap<Integer, Animal> getAnimalCycle() {
-        return AnimalCycle;
-    }
 
-    public void setAnimalCycle(HashMap<Integer, Animal> animalCycle) {
-        AnimalCycle = animalCycle;
-    }
 
-    public HashMap<Integer, Threat> getThreatCycle() {
-        return ThreatCycle;
-    }
 
-    public void setThreatCycle(HashMap<Integer, Threat> threatCycle) {
-        ThreatCycle = threatCycle;
-    }
-
-    public HashMap<Integer, Facility> getFacilityCycle() {
-        return FacilityCycle;
-    }
-
-    public void setFacilityCycle(HashMap<Integer, Facility> facilityCycle) {
-        FacilityCycle = facilityCycle;
-    }
 
     public HashMap<Enum, Integer> getLevelGoals() {
         return LevelGoals;
