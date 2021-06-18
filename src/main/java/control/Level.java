14f74da873b1c69434c86d6a205713e646b2661a
alinkkh <alinkkh9@gmail.com>
2021-06-18 17:31:37 +0430

    LevelArrayList Completed

--- src/main/java/control/Level.java
+++ src/main/java/control/Level.java
@@ -1,10 +1,19 @@
 package control;
 
+import model.Animal;
+import model.Facility;
+import model.Threat;
+
+import java.util.HashMap;
+
 public class Level {
     private int level,goldTime,silverTime,bronzeTime,goldStars,silverStars,bronzeStars;
+    private HashMap<Integer,Animal> AnimalCycle;
+    private HashMap<Integer, Threat> ThreatCycle;
+    private HashMap<Integer, Facility> FacilityCycle;
     // All data of a level should be here
 
-    public Level(int level, int goldTime, int silverTime, int bronzeTime, int goldStars, int silverStars, int bronzeStars) {
+    public Level(int level, int goldTime, int silverTime, int bronzeTime, int goldStars, int silverStars, int bronzeStars,HashMap animalCycle,HashMap threatCycle,HashMap facilityCycle) {
         this.level = level;
         this.goldTime = goldTime;
         this.silverTime = silverTime;
@@ -12,6 +21,9 @@ public class Level {
         this.goldStars = goldStars;
         this.silverStars = silverStars;
         this.bronzeStars = bronzeStars;
+        this.AnimalCycle=animalCycle;
+        this.ThreatCycle=threatCycle;
+        this.FacilityCycle=facilityCycle;
     }
 
     public int getLevel() {
@@ -68,5 +80,29 @@ public class Level {
 
     public void setBronzeStars(int bronzeStars) {
         this.bronzeStars = bronzeStars;
+    }
+
+    public HashMap<Integer, Animal> getAnimalCycle() {
+        return AnimalCycle;
+    }
+
+    public void setAnimalCycle(HashMap<Integer, Animal> animalCycle) {
+        AnimalCycle = animalCycle;
+    }
+
+    public HashMap<Integer, Threat> getThreatCycle() {
+        return ThreatCycle;
+    }
+
+    public void setThreatCycle(HashMap<Integer, Threat> threatCycle) {
+        ThreatCycle = threatCycle;
+    }
+
+    public HashMap<Integer, Facility> getFacilityCycle() {
+        return FacilityCycle;
+    }
+
+    public void setFacilityCycle(HashMap<Integer, Facility> facilityCycle) {
+        FacilityCycle = facilityCycle;
     }
 }
